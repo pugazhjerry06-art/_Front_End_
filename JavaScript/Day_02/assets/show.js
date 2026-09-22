249f175056d3a5e78ae5092428c75a74f7245bfd
pugazhjerry06-art <pugazhjerry06@gmail.com>
2026-09-22 16:43:43 +0530

    upload

---
+++ JavaScript/Day_02/assets/show.js
@@ -0,0 +1,129 @@
+var userName = 10;
+var onShow = 10;
+var dataType= 10;
+var onBoard = 10;
+var onDisplay = 10;
+var onClick = 10;
+var showDisplay = 10;
+var onSpace = 10;
+var typeShow= 10;
+var showData = 10;
+var onScreen = 10;
+var displayScreen = 10;
+var infunction = 10;
+var homePage = 10;
+var webPage = 10;
+var webSite = 10;
+var backDoor = 10;
+var dateShow = 10;
+var showType = 10;
+var showPage = 10;
+var showSite = 10;
+var onbutton = 10;
+var onBack = 10;
+var onDelete = 10;
+var backSpace = 10;
+var searchBox = 10;
+var capsLoack = 10;
+var onMouse = 10;
+var mouseMove = 10;
+var clickTO = 10;
+
+
+
+var userName = 20;
+console.log(userName);
+console.log(userName);
+console.log(userName);
+console.log(userName);
+console.log(userName);
+
+
+var userName = "pugazh";
+console.log(userName);
+
+var userName = "arun";
+console.log(userName);
+
+var userName = 10;
+console.log(userName);
+
+var userName = 8;
+console.log(userName);
+
+var userName = true;
+console.log(userName);
+
+var userName = false;
+console.log(userName);
+
+var userName = null;
+console.log(userName);
+
+var userName = null;
+console.log(userName);
+
+var x;
+console.log(x);
+
+var x;
+console.log(x);
+
+
+
+let UserName = "magesh" ;
+console.log(UserName);
+
+let OnShow = "arun";
+console.log(OnShow);
+
+let OnClick = 8;
+console.log(OnClick);
+
+let OnSpace = 10;
+console.log( OnSpace);
+
+let OnDisplay = true;
+console.log(OnDisplay);
+
+let OnUser = false;
+console.log(OnUser);
+
+let DataType = null;
+console.log(DataType);
+
+let TypeShow = null;
+console.log(TypeShow);
+
+let y;
+console.log(x);
+let z;
+console.log(x);
+
+
+const User_Name = "magesh" ;
+console.log(User_Name);
+
+const On_Show = "arun";
+console.log(On_Show);
+
+const On_Click = 8;
+console.log(On_Click);
+
+const On_Space = 10;
+console.log( On_Space);
+
+const On_Display = true;
+console.log(On_Display);
+
+const On_User = false;
+console.log(On_User);
+
+const Data_Type = null;
+console.log(Data_Type);
+
+const Type_Show = null;
+console.log(Type_Show);
+
+
+
